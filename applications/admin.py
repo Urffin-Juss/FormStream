db3fb0c757e531e5f341ef6ef5141ddf1bc52cc0
Urffin-Juss <im@maxsuns.ru>
2026-07-05 20:37:44 +0300

    add registration to admin panel

--- applications/admin.py
+++ applications/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from applications.models import Application
 
-# Register your models here.
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "operation_type",
+        "client_name",
+        "inn",
+        "status",
+    )
+
+    list_filter = (
+        "operation_type",
+        "payment_type",
+        "status",
+    )
+
+    search_fields = (
+        "client_name",
+        "inn",
+        "kpp",
+        "contact_phone",
+    )
